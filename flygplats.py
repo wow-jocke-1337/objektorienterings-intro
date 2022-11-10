67005bafc9b5fcc98772e58bf0b1badf5b6a1ad7
wow-jocke-1337 <Alfred.hogerberg@live.se>
2022-11-10 10:21:02 +0100

    övning

--- flygplats.py
+++ flygplats.py
@@ -19,6 +19,11 @@ class Airplane():
         print(
             f"{self.model}. Wingspan: {self.wingspan}. Capacity: {self.capacity}")
 
+    def set_capacity(self, new_capacity):
+        self.capacity = new_capacity
+
+    def get_capacity(self):
+        return self.capacity
 
 # ----------Huvudprogram----------
 # Nu när klassen finns kan vi skapa objekt (variabler) med denna typ.
@@ -26,3 +31,11 @@ class Airplane():
 a_plane = Airplane("Boeing 747", 64.6, 500)
 a_plane.model = 'Airbus 380'
 a_plane.print_info()
+
+b_plane = Airplane("SAS 420", 69, 1000)
+b_plane.print_info()
+b_plane.set_capacity(2000)
+b_plane.print_info()
+b_plane.get_capacity()
+the_capacity = b_plane.get_capacity()
+print(the_capacity)
